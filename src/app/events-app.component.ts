f009c26065f45321b6214dca5b7cc669c5974d07
Madan Kommera <Madan.Kommera@globalservs.com>
2018-11-26 17:28:16 +0000

    initial commit

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'events-app',
+  template: `<events-list></events-list>
+  `
+})
+export class EventsAppComponent {
+  title = 'App' ;
+}
